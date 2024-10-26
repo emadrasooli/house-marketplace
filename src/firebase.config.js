a740e3bb127bb04f829849e6fa93e11063de3ae1
M.Emad Rasooli <rasooliemad2004@gmail.com>
2024-10-26 16:36:28 +0430

    3-setUp-Authentication-with-email

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,4 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase
 export const db = getFirestore();
+export const auth = getAuth(app);
