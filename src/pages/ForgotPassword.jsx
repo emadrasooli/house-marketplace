d90b0702491019f70087e9bb103fbf865b145a53
M.Emad Rasooli <rasooliemad2004@gmail.com>
2024-10-27 16:25:38 +0430

    4-setUp-User-Profile

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { getAuth, sendPasswordResetEmail } from "firebase/auth"
+import { toast } from "react-toastify"
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+
 function ForgotPassword() {
+  const onChange = e => {
+
+  }
+
+  const onSubmit = e => [
+    e.preventDefault()
+  ]
+
     return (
       <div>
         <h1>Forgot Password</h1>
